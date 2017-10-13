4a9ab8573c3ad706959a4116cab607ff6401c9d7
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-10-13 23:59:25 +0300

    Add lock table

--- DBConnect.py
+++ DBConnect.py
@@ -9,13 +9,10 @@ class DBService:
     def __del__(self):
         self.db.close()
 
-    def get_url(self):
-        self.cur.execute('SELECT url FROM urls limit 1')
+    def get_url(self, n=1):
+        self.cur.execute('SELECT get_url({0})'.format(n))
         result = self.cur.fetchone()
-        if result is not None:
-            self.cur.execute('DELETE FROM urls WHERE url=\'{0}\''.format(result[0]))
-            self.db.commit()
-        return result
+        return result[0]
 
     def is_empty(self):
         self.cur.execute('SELECT COUNT(*) FROM urls')
@@ -23,13 +20,16 @@ class DBService:
         return result[0] == 0
 
     def add_url(self, urls):
-            for url in urls:
-                try:
+        for url in urls:
+            try:
+                cmd = 'SELECT url FROM old_urls WHERE url=\'{0}\''.format(url)
+                self.cur.execute(cmd)
+                if self.cur.fetchone() is None:
                     cmd = 'INSERT INTO urls(url) VALUES (\'{0}\')'.format(url)
                     self.cur.execute(cmd)
                     self.db.commit()
-                except:
-                    self.db.rollback()
+            except:
+                self.db.rollback()
 
     def size(self):
         self.cur.execute('SELECT COUNT(*) FROM storage')
@@ -41,11 +41,11 @@ class DBService:
         count = self.cur.fetchone()
         if count[0] == 0:
             cmd = 'INSERT INTO storage(url, update_count, last_update, bdata) VALUES (\'{0}\', 0, now(), {1})'\
-                .format(url, pg_driver.Binary(data.encode()))
+                .format(url, pg_driver.Binary(data))
             self.cur.execute(cmd)
             self.db.commit()
         else:
             cmd = 'UPDATE storage SET update_count=update_count+1, last_update=now(), bdata={0} WHERE url=\'{1}\''\
-                .format(pg_driver.Binary(data.encode()), url)
+                .format(pg_driver.Binary(data), url)
             self.cur.execute(cmd)
             self.db.commit()
