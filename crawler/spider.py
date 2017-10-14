5a9b7c5c43bb73878c1af613121f1c9a29c2425a
Vadim Kleiman <vadim.kleiman@gmail.com>
2017-10-14 15:05:38 +0300

    Fix some bugs

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,69 +1,63 @@
-import urllib
-import urllib.error
-from html.parser import HTMLParser
-from urllib import parse
-from urllib.request import urlopen
+import requests
+from bs4 import BeautifulSoup
+from bs4.dammit import EncodingDetector
+import urllib.parse
 import logging
-from url_modify import asciify_url
 import time
 import reppy
+import re
+
+headers = {'User-Agent': 'MedBot', 'Content-type': 'text/html'}
+
+robots_agent = {}
 
 
-class LinkParser(HTMLParser):
-    def __init__(self, db_cursor):
-        super().__init__()
-        self.__baseUrl = ''
-        self.__links = []
-        self.__db_cursor = db_cursor
-        self.__agent_name = "MedBot"
-        self.__robots_agent = {}
-
-    def error(self, message):
-        super(LinkParser, self).error(message)
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            for (key, value) in attrs:
-                if key == 'href':
-                    new_url = parse.urljoin(self.__baseUrl, value)
-                    self.__links.append(new_url)
-
-    def parse(self, url):
-        robots_url = reppy.Robots.robots_url(url)
-        if url not in self.__robots_agent:
-            robots = reppy.Robots.fetch(robots_url)
-            agent = robots.agent(self.__agent_name)
-            self.__robots_agent[robots_url] = agent
-        agent = self.__robots_agent[robots_url]
-
-        if not agent.allowed(url):
-            logging.info("Disallow crawling " + url)
-            return
-        self.__baseUrl = url
-        self.__links = []
+def get_urls(db_cursor, url):
+    try:
+        response = requests.get(url, headers=headers)
         time.sleep(1)
-        try:
-            response = urlopen(asciify_url(url))
-        except urllib.error.URLError as e:
-            logging.warning(msg=e.reason)
-            return
-
-        if 'text/html' in response.getheader('Content-Type'):
-            data = response.read()
-            self.feed(data.decode(response.headers.get_content_charset()))
-            self.__db_cursor.add_data(url, data)
-
-    def get_links(self):
-        return self.__links
+        http_encoding = response.encoding if 'charset' in response.headers.get('content-type', '').lower() else None
+        html_encoding = EncodingDetector.find_declared_encoding(response.content, is_html=True)
+        encoding = html_encoding or http_encoding
+        soup = BeautifulSoup(response.content, "lxml", from_encoding=encoding)
+        result = []
+        db_cursor.add_data(url, soup.text.encode())
+        for link in soup.find_all('a', href=True):
+            result.append(urllib.parse.urljoin(url, link['href']))
+        return result
+    except requests.exceptions.ReadTimeout:
+        logging.error("Read timeout")
+    except requests.exceptions.ConnectTimeout:
+        logging.error("Connect timeout")
+    except:
+        logging.error("Exception")
 
 
 def spider(db_cursor):
-    parser = LinkParser(db_cursor)
     pages = db_cursor.get_url(10)
     while pages:
         for page in pages:
+            regex = re.compile(
+                r'^(?:http|ftp)s?://'
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+                r'localhost|' 
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+                r'(?::\d+)?'
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+            m = regex.match(page)
+            if not m:
+                logging.info("Invalid url " + page)
+                continue
+            robots_url = reppy.Robots.robots_url(page)
+            if page not in robots_agent:
+                robots = reppy.Robots.fetch(robots_url)
+                agent = robots.agent(headers["User-Agent"])
+                robots_agent[robots_url] = agent
+            agent = robots_agent[robots_url]
+            if not agent.allowed(page):
+                logging.info("Disallow crawling " + page)
+                continue
             links = []
-            parser.parse(page)
-            links.extend(parser.get_links())
+            links.extend(get_urls(db_cursor, page))
             db_cursor.add_url(links)
         pages = db_cursor.get_url(10)
